80d5078da365103f7e6e2dc6fcb171d450e0ed9d
Maksym Galchuk <maksymgalch@gmail.com>
2024-06-14 01:45:19 +0300

    Fix boilerplate

--- wp-content/themes/johndoe/webpack.config.js
+++ wp-content/themes/johndoe/webpack.config.js
@@ -4,67 +4,78 @@ const webpack = require("webpack");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-const isDev = false;
+module.exports = (env) => {
+  const isDev = env.development;
+  const phpServer = env.phpServer;
 
-const config = {
-  mode: isDev ? "development" : "production",
-  devtool: isDev ? "source-map" : false,
-  entry: {
-    main: [
-      "./src/js/main.js",
-      "./src/scss/main.scss"
-    ]
-  },
-  output: {
-    filename: "assets/js/main.[contenthash].js",
-    path: path.resolve(__dirname),
-  },
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [
-        "./assets/js/*",
-        "./assets/css/*",
+  return {
+    mode: isDev ? "development" : "production",
+    devtool: isDev ? "source-map" : false,
+    entry: {
+      main: [
+        "./src/js/main.js",
+        "./src/scss/main.scss"
       ]
-    }),
-    new BrowserSyncPlugin(
-      {
-        host: "localhost",
-        port: 3000,
-        proxy: "http://wds-test/",
-        files: [
-          "./assets/js/*.js",
-          "./assets/css/*.css",
-          "./**/*.php",
-        ],
+    },
+    output: {
+      filename: "assets/js/main.[contenthash].js",
+      path: path.resolve(__dirname),
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: [
+          "./assets/js/*",
+          "./assets/css/*",
+        ]
       }),
-    new MiniCssExtractPlugin({ filename: "./assets/css/main.[contenthash].css" }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "./assets/fonts/[name][ext]"
-        }
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "./assets/img/[name][ext]",
-        },
-      },
+      new BrowserSyncPlugin(
+        {
+          host: "localhost",
+          port: 3000,
+          proxy: phpServer,
+          files: [
+            "./assets/js/*.js",
+            "./assets/css/*.css",
+            "./**/*.php",
+          ],
+        }),
+      new MiniCssExtractPlugin({ filename: "./assets/css/main.[contenthash].css" }),
     ],
-  },
-  resolve: {
-    extensions: [".js"],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "resolve-url-loader",
+            {
+              loader: "sass-loader",
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          type: "asset/resource",
+          generator: {
+            filename: "./assets/fonts/[name][ext]"
+          }
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "./assets/img/[name][ext]",
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js"],
+    },
+  };
 };
-
-module.exports = config;
